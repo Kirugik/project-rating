564085e94ef75eef0382aff0f942e22ac1fbd225
Robert Kirui <kirugik79@gmail.com>
2022-06-11 17:17:53 +0300

    Feat: Add view functions for index, project-details, project-review, new-project, search-project, and user-profile pages

--- rater/views.py
+++ rater/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, 'rater/index.html')
+
+
+def project_details(request):
+    return render(request, 'rater/project_details.html')
+
+
+def project_review(request):
+    return render(request, 'rater/project_review.html')
+
+
+def search_project(request):
+    return render(request, 'rater/search.html')
+
+
+def new_project(request):
+    return render(request, 'rater/new_project.html') 
+
+
+def user_profile(request):
+    return render(request, 'rater/user_profile.html')
+
+
+def update_profile(request):
+    return render(request, 'rater/update_profile.html') 
+
+
