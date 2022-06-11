90faa0a34389f3eb6a35ea85d337998f4ea4134c
Robert Kirui <kirugik79@gmail.com>
2022-06-12 02:26:14 +0300

    Feat: Configure url to handle API requests for profiles and projects

--- rater/urls.py
+++ rater/urls.py
@@ -6,13 +6,19 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
     path('project-details/', views.project_details, name='project-details'),
     path('project-review/', views.project_review, name='project-review'),
     path('new-project/', views.new_project, name='new-project'),
+
     path('search/', views.search_project, name='search'),
+
     path('user-profile/', views.user_profile, name='user-profile'),
     path('update-profile/', views.project_details, name='update-profile'), 
-]
+
+    path('projectrating/api/profiles/', views.ProfilesList.as_view(), name='profiles-api'),
+    path('projectrating/api/projects/', views.ProjectsList.as_view(), name='projects-api'), 
+]  
 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
